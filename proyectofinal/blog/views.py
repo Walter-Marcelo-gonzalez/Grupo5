53b7f31c54ac076b981412c774474c625217ddf6
Walter-Marcelo-Gonzalez <walterwalter1976@gmail.com>
2022-08-07 00:40:26 -0300

    borro linea inecesaria por importar 2 veces HttpResponse

--- proyectofinal/blog/views.py
+++ proyectofinal/blog/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+# (Walter) Por ahora comento que la linea de arriba se puede eliminar porque
+# en la linea de abajo, HttpResponse ya esta importado, por lo tanto
+# es inecesaria, por ahora dejo el comentario para que se ubiquen, pero luego
+# borramos todo esto.
+# El que ve pone su nombre abajo y cuando todos lo vean borramos
+#
+
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
